e52ba9a54b5c91d38f44000041c3aec7afca48b9
Dmitriy Raschupkin <d.s.raschupkin@gmail.com>
2022-08-27 20:42:09 +0300

    Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int FirstDigit = a / 10;
+int SecodDigit = FirstDigit % 10;
+
+Console.WriteLine(SecodDigit);
